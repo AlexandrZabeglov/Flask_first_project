1160f4d6153f77f583077c1365610363a79f2665
Alexandr Zabegalov <alexandrzabegalov@mail.ru>
2022-12-23 16:50:50 +0300

    commit 1.0

---
+++ requests.py
@@ -0,0 +1,10 @@
+import requests
+
+HOST = 'http://127.0.0.1:8000'
+
+# response = requests.post(f'{HOST}/ads/',json={'article':'hi1hi','text':'ertert','owner':'ertert'})
+# response = requests.get(f'{HOST}/ads/1')
+# response = requests.patch(f'{HOST}/ads/1',json={'article':'try patch'})
+# response = requests.delete(f'{HOST}/ads/2')
+
+# print(response.status_code)
